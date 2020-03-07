d0731c14e167906af52b140061bef0bcc006f659
james goldswain <james.goldswain@readify.net>
2020-03-08 00:24:51 +0800

    Cleanup and styling

--- components/Domino/index.tsx
+++ components/Domino/index.tsx
@@ -1,71 +1,15 @@
 /** @jsx jsx */
 import { jsx } from '@emotion/core'
-import { Formik, FormikHelpers } from 'formik';
 import { styles } from './styles';
-import { Domino } from '../../interfaces/Domino';
+import { Emoji } from '../../lib/icons';
+import { IDomino } from '../../interfaces/IDomino';
 
-interface Errors {
-    email: string
-}
-
-const NewDomino = ({handleSubmit}: any) => {
+const Domino = ({domino} : any) => {
   return (
+    
     <div css={styles}>
-    <h1>Add a domino!</h1>
-    <Formik
-      initialValues={{ colour: 'blue', value: 1, icon: '' } as Domino}
-      onSubmit={(domino: Domino, actions) => {
-        handleSubmit(domino);
-        actions.setSubmitting(false);
-      }}
-    >
-      {({
-        values,
-        errors,
-        touched,
-        handleChange,
-        handleBlur,
-        handleSubmit,
-        isSubmitting,
-      }) => (
-        <form onSubmit={handleSubmit}>
-          <select
-            name="icon"
-            value={values.icon}
-            onChange={handleChange}
-            onBlur={handleBlur}
-            style={{ display: 'block' }}
-          >
-            <option value="" label="Select" />
-            <option value="🍕" label="🍕" />
-            <option value="☕" label="☕" />
-            <option value="🌭" label="🌭" />
-            <option value="🥓" label="🥓" />
-            <option value="🍉" label="🍉" />
-          </select>
-          <input
-            type='text'
-            name='colour'
-            onChange={handleChange}
-            onBlur={handleBlur}
-            value={values.colour}
-          />
-          {errors.colour && touched.colour && errors.colour}
-          <input
-            type='number'
-            name='value'
-            onChange={handleChange}
-            onBlur={handleBlur}
-            value={values.value}
-          />
-          {errors.value && touched.value && errors.value}
-          <button type='submit' disabled={isSubmitting}>
-            Submit
-          </button>
-        </form>
-      )}
-    </Formik>
-  </div>);
+      <div className='top'>{domino.topIcon}</div>
+      <div className='bottom'>{domino.bottomIcon}</div>
+    </div>);
 }
-
-export default NewDomino;
+export default Domino;
